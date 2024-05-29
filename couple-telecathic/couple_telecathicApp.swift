17410e42c947080dd811e1ad817431ce823f3d45
ssabrut <michael.gunawan2002@gmail.com>
2024-05-29 09:19:29 +0700

    Initial Commit

---
+++ couple-telecathic/couple_telecathicApp.swift
@@ -0,0 +1,32 @@
+//
+//  couple_telecathicApp.swift
+//  couple-telecathic
+//
+//  Created by Michael Eko on 29/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct couple_telecathicApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
